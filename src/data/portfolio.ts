c942204bf3a39051b1c934b81e39f9c33779113e
davimaniibrahim <davimaniibrahim@gmail.com>
2025-06-09 03:36:50 +0100

    feat:added image url for wedding and other categories

--- src/data/portfolio.ts
+++ src/data/portfolio.ts
@@ -14,38 +14,31 @@ export const portfolioItems: PortfolioItem[] = [
     id: 'p1',
     title: 'Professional Headshot',
     category: 'portrait',
-    image: '/https://res.cloudinary.com/dikmotd52/image/upload/v1749349118/EAS_8835_rgckyo.jpg',
+    image: 'https://res.cloudinary.com/dikmotd52/image/upload/v1749434749/3_6_kflmoo.jpg',
     description: 'Professional business portrait with natural lighting'
   },
   {
     id: 'p2',
     title: 'Creative Portrait',
     category: 'portrait',
-    image: 'https://images.pexels.com/photos/1130626/pexels-photo-1130626.jpeg?auto=compress&cs=tinysrgb&w=800',
+    image: 'https://res.cloudinary.com/dikmotd52/image/upload/v1749434758/3_5_ugech2.jpg',
     description: 'Artistic portrait with dramatic lighting'
   },
   {
     id: 'p3',
     title: 'Family Portrait',
     category: 'portrait',
-    image: 'https://images.pexels.com/photos/1682497/pexels-photo-1682497.jpeg?auto=compress&cs=tinysrgb&w=800',
+    image: 'https://res.cloudinary.com/dikmotd52/image/upload/v1749434758/3_5_ugech2.jpg',
     description: 'Beautiful family moment captured naturally'
   },
-  // Pre-wedding
-  {
-    id: 'pw1',
-    title: 'Romantic Garden Session',
-    category: 'prewedding',
-    image: 'https://images.pexels.com/photos/1024993/pexels-photo-1024993.jpeg?auto=compress&cs=tinysrgb&w=800',
-    description: 'Romantic pre-wedding shoot in natural garden setting'
-  },
+
   
   // Wedding
   {
     id: 'w1',
     title: 'Wedding Ceremony',
     category: 'wedding',
-    image: 'https://images.pexels.com/photos/1024954/pexels-photo-1024954.jpeg?auto=compress&cs=tinysrgb&w=800',
+    image: 'https://res.cloudinary.com/dikmotd52/image/upload/v1749434755/3_7_moa8bh.jpg',
     description: 'Beautiful wedding ceremony moments'
   },
   {
@@ -68,21 +61,21 @@ export const portfolioItems: PortfolioItem[] = [
     id: 'e1',
     title: 'Corporate Event',
     category: 'event',
-    image: 'https://images.pexels.com/photos/1181406/pexels-photo-1181406.jpeg?auto=compress&cs=tinysrgb&w=800',
+    image: 'https://res.cloudinary.com/dikmotd52/image/upload/v1749434776/3_30_zifwqg.jpg',
     description: 'Professional corporate event coverage'
   },
   {
     id: 'e2',
     title: 'Birthday Celebration',
     category: 'event',
-    image: 'https://images.pexels.com/photos/1729798/pexels-photo-1729798.jpeg?auto=compress&cs=tinysrgb&w=800',
+    image: 'https://res.cloudinary.com/dikmotd52/image/upload/v1749434788/3_2_oiawtc.jpg',
     description: 'Joyful birthday celebration moments'
   },
   {
     id: 'e3',
     title: 'Conference Coverage',
     category: 'event',
-    image: 'https://images.pexels.com/photos/1181534/pexels-photo-1181534.jpeg?auto=compress&cs=tinysrgb&w=800',
+    image: 'https://res.cloudinary.com/dikmotd52/image/upload/v1749434764/3_24_kqbwwx.jpg',
     description: 'Professional conference and seminar photography'
   }
 ];
